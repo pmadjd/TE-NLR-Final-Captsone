6bace8ab139fd8a0ffb38254dbf237af802b60ca
Kyle David <kdavid9211@gmail.com>
2021-12-07 11:37:53 -0500

    Kyle push 1137ET 1207

---
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -0,0 +1,25 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.UserDao;
+import com.techelevator.security.jwt.TokenProvider;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class PetController {
+    private UserDao userDao;
+
+    public PetController(UserDao userdao) {
+        this.userDao = userdao;
+    }
+
+//    @RequestMapping(path = "/pets", method = RequestMethod.GET)
+//    public String getPets(Principal principal) {
+//
+//    }
+
+}
