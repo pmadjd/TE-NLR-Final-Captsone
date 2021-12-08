10a76aa7a0644bbf99da90d2b781ae10535423e3
Pareesa Madjd <pareesa.madjd@gmail.com>
2021-12-08 10:18:38 -0500

    home view updates

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>
+      <img
+        src="https://i.ibb.co/rbyMTpM/Image-from-i-OS-8.png"
+        alt="shelter logo"
+        id="logo"
+      />
+    </h1>
     <!--home is a placeholder for logo-->
     <img
+      src="https://i.ibb.co/PGMJcjz/dog-and-cat-banner-23.png"
+      alt="shelter banner"
+      border="0"
       id="banner"
-      src = "https://animalhaven.org/wp-content/uploads/2015/12/dog-and-cat-banner-2.png"
-      alt="cat dog banner"
     />
 
     <div class="well-display">
@@ -14,8 +21,8 @@
           src="https://www.montgomerycountymd.gov/animalservices/Resources/Images/dogicon.png"
           alt="dog icon"
         />
-        <router-link v-bind:to="{name: 'dogs'}">
-        <div class="well-text">VIEW DOGS</div>
+        <router-link v-bind:to="{ name: 'dogs' }">
+          <div class="well-text">VIEW DOGS</div>
         </router-link>
       </div>
       <div class="well">
@@ -23,17 +30,17 @@
           src="https://www.montgomerycountymd.gov/animalservices/Resources/Images/caticon.png"
           alt="cat icon"
         />
-         <router-link v-bind:to="{name: 'cats'}">
-        <div class="well-text">VIEW CATS</div>
-         </router-link>
+        <router-link v-bind:to="{ name: 'cats' }">
+          <div class="well-text">VIEW CATS</div>
+        </router-link>
       </div>
       <div class="well">
         <img
           src="https://www.montgomerycountymd.gov/animalservices/Resources/Images/othericon.png"
           alt="other pets icon"
         />
-        <router-link v-bind:to="{name: 'others'}">
-        <div class="well-text">VIEW OTHERS</div>
+        <router-link v-bind:to="{ name: 'others' }">
+          <div class="well-text">VIEW OTHERS</div>
         </router-link>
       </div>
     </div>
@@ -50,6 +57,10 @@ export default {
 h1 {
   text-align: center;
 }
+#logo {
+  width: 12%;
+  height: auto;
+}
 div.well-display {
   display: flex;
   justify-content: center;
@@ -59,15 +70,17 @@ div.well-display {
   font-weight: bold;
   font-family: "Nunito Sans", sans-serif;
 }
-div.well{
+div.well {
   margin: 20px;
   margin-top: 2px;
 }
-.well-text{
+.well-text {
   text-decoration: none;
 }
 #banner {
   width: 100%;
   height: auto;
+  display: flex;
+  justify-content: center;
 }
 </style>
