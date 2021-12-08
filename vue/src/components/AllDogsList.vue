afac0beafd4bc939c9e03b642c910ef097da1f72
Pareesa Madjd <pareesa.madjd@gmail.com>
2021-12-08 18:44:59 -0500

    a few more front end updates

--- vue/src/components/AllDogsList.vue
+++ vue/src/components/AllDogsList.vue
@@ -10,7 +10,7 @@
     <div class="dog" v-for="dog in dogs" v-bind:key="dog.petId">
       <h2 class="pet-name">{{ dog.petName }}</h2>
       <h3 class="pet-id">Pet ID: {{ dog.petId }}</h3>
-      <img src="{dog.petPhoto}" alt="dog photo" />
+      <img v-bind:src="dog.petPhoto" alt="dog photo" id="pet-photo" />
       <!-- ??????? -->
       <h3 class="pet-gender">Gender: {{ dog.petGender }}</h3>
       <h3 class="pet-breed">Breed: {{ dog.petBreed }}</h3>
@@ -33,6 +33,7 @@ export default {
   },
   created() {
     petService.getDogList().then((response) => {
+      console.log(response.data);
       this.dogs = response.data;
     });
   },
@@ -40,15 +41,26 @@ export default {
 </script>
 
 <style>
+#pet-photo {
+  width: 75%;
+  height: auto;
+  border-radius: 15px;
+}
 .dog {
-  border: 2px solid black;
-  border-radius: 10px;
+  border: 2px solid rgb(112, 85, 28);
+  border-radius: 15px;
+  border-width: 2px;
   width: 250px;
-  height: 550px;
+  height: 700px;
   margin: 20px;
   text-align: center;
+  background-color: rgb(221, 240, 247);
+  color: rgb(38, 47, 121);
 }
 
+.pet-description {
+  margin: 20px;
+}
 .dog-card {
   display: flex;
   justify-content: space-evenly;
